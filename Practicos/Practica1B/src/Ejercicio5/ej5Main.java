378fededc613f54cbed490d6c0975d037135dadd
Felipe Massera <felipemassera88@gmail.com>
2023-03-19 21:41:40 -0300

    cargo practica 1b ejercicio 5

--- Practicos/Practica1B/src/Ejercicio5/ej5Main.java
+++ Practicos/Practica1B/src/Ejercicio5/ej5Main.java
@@ -11,6 +11,43 @@ package Ejercicio5;
  */
 public class ej5Main {
 
+    public static String incisoA(int[] vect) {
+        int max = 0, min = 9999, total = 0, aux;
+
+        for (int i = 0; i < vect.length; i++) {
+            aux = vect[i];
+            if (aux > max) {
+                max = aux;
+            }
+            if (aux < min) {
+                min = aux;
+            }
+            total += aux;
+        }
+        double promedio = (double) (total) / (double) (vect.length);
+        return ("numero maximo: " + max + "\n numero minimo: " + min + "\n promedio: " + promedio);
+    }
+
+    public static incisoB metodoB(incisoB B, int[] vect) {
+        int max = 0, min = 9999, total = 0, aux;
+
+        for (int i = 0; i < vect.length; i++) {
+            aux = vect[i];
+            if (aux > max) {
+                max = aux;
+            }
+            if (aux < min) {
+                min = aux;
+            }
+            total += aux;
+        }
+        double promedio = (double) (total) / (double) (vect.length);
+        B.setMax(max);
+        B.setMin(min);
+        B.setPromedio(promedio);
+        return B;
+    }
+
     public static void cargarArray(int[] vect, int x) {
         for (int i = 0; i < x; i++) {
             vect[i] = (int) (Math.random() * 25 + 1);
@@ -30,9 +67,14 @@ public class ej5Main {
         cargarArray(array, x);
         mostrarArray(array, x);
         //inciso A
-        
-        
+        System.out.println("Inciso A: \n" + incisoA(array));
+        //inciso b
+        incisoB B = new incisoB();
+        metodoB(B, array);
+        System.out.println(B.toString());
+        //inciso c
+        incisoB C = new incisoB();
+        C.incisoC();
+        System.out.println("Inciso C \n max: "+C.getMax() +"\n min: "+C.getMin()+"\n promedio: "+C.getPromedio());
     }
-
 }
-
---
+++ Practicos/Practica1B/src/Ejercicio5/incisoB.java
@@ -0,0 +1,78 @@
+package Ejercicio5;
+
+public class incisoB {
+
+    private int min;
+    private int max;
+    private double promedio;
+
+    public incisoB() {
+    }
+
+    public incisoB(int min, int max, double promedio) {
+        this.min = min;
+        this.max = max;
+        this.promedio = promedio;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "incisoB{" + "min=" + min + ", max=" + max + ", promedio=" + promedio + '}';
+    }
+
+    public void incisoC() {
+        int[] vect = new int[10];
+        
+    //cargo el vector
+        for (int i = 0; i < vect.length; i++) {
+            vect[i] = (int) (Math.random() * 25 + 1);
+        }
+    //muestro
+        System.out.println("Muestro nuevo vector INCISO C");    
+    for (int i = 0; i < vect.length; i++) {
+            System.out.println((i + 1) + "- " + vect[i]);
+        }
+    // calculo max, min y promedio
+        int max = 0, min = 9999, total = 0, aux;
+
+        for (int i = 0; i < vect.length; i++) {
+            aux = vect[i];
+            if (aux > max) {
+                max = aux;
+            }
+            if (aux < min) {
+                min = aux;
+            }
+            total += aux;
+        }
+        double promedio = (double) (total) / (double) (vect.length);
+        this.min = min;
+        this.max = max;
+        this.promedio = promedio;
+    }
+
+}
