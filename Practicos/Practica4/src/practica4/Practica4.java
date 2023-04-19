022202c9bb1eda23879ee55ab00bc781aeb78531
Felipe Massera <felipemassera88@gmail.com>
2023-04-19 19:39:28 -0300

    incorporo tp4

---
+++ Practicos/Practica4/src/practica4/Practica4.java
@@ -0,0 +1,18 @@
+/*
+ */
+package practica4;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Practica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
